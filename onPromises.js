c2fe2e0f3eb50139cc2ef843f27bb6a0a593cb12
Semen Dorofeev <sdorofeev@changers.team>
2017-04-01 16:41:01 +0300

    finished

--- onPromises.js
+++ onPromises.js
@@ -2,6 +2,8 @@
 var request = require('request');
 var fs = require('fs');
 
+
+
 function first(resolve,reject) {
     request("http://api.population.io:80/1.0/countries", function (error, response, body) {
         resolve(body);
@@ -11,32 +13,45 @@ function first(resolve,reject) {
 
 function second(json) {
     var countriesObj = JSON.parse(json);
-    var country = countriesObj.countries[0];
+    var array = [];
+    for(var i = 0; i < 50;i++){
+        let country = countriesObj.countries[i];
+        array.push(new Promise(function (resolve, reject) {
+            request('http://api.population.io:80/1.0/population/1980' + country + '/', function (error, response, body) {
+                resolve({text: body,country : country});
+            });
+        }));
+    }
 
 
-   return new Promise(function (resolve, reject) {
-       request('http://api.population.io:80/1.0/population/1980' + country + '/', function (error, response, body) {
-           resolve({text: body,country : country});
-       });
-   });
+
+   return array;
 };
 
-function three(obj) {
+function three(arrayOfPromises) {
+    console.log(arrayOfPromises);
+    arrayOfPromises.forEach(function (item,index,array) {
+      item.then(four).then(five);
+    });
+
+
+};
+
+function four(obj) {
     var nameOfFile = 'test'+ obj.country  + '.txt';
     var writer = fs.createWriteStream(nameOfFile, {flags: 'w'});
     writer.write(obj.text);
-    console.log('ff');
+    console.log(nameOfFile);
     return  nameOfFile;
 
+}
 
-};
-
-function four(path) {
+function five(path) {
     fs.readFile(path,'utf8',function (err, content) {
-        console.log(content);
+        console.log('читаю');
     });
 };
 
 var promise = new Promise(first);
 
-promise.then(second).then(three).then(four);
+promise.then(second).then(three);
